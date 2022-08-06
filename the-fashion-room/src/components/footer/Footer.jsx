d9d545f6752fb9dddbf179daa484c0d64b039260
Brayan <brayan.sanchez.leon@correounivalle.edu.co>
2022-08-06 17:24:54 -0500

    Footer done

--- the-fashion-room/src/components/footer/Footer.jsx
+++ the-fashion-room/src/components/footer/Footer.jsx
@@ -1,13 +1,116 @@
 import React from "react";
 import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
 import "./Footer.css";
+import { Container, Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 function Footer() {
   return (
-    <div className="app_footer">
-      Footer
-      <FacebookIcon />
-    </div>
+    <Box className="app_footer">
+      <Container>
+        <Grid container spacing={3}>
+          <Grid item container xs={3}>
+            <Grid item xs={12}>
+              <Typography component="div" className="title_footer">
+                Conócenos
+              </Typography>
+            </Grid>
+            <Grid item container direction="column">
+              <Grid item>
+                <Typography component="div" className="link_footer">
+                  Trabaja en The Fashion Room
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography component="div" className="link_footer">
+                  Blog
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography component="div" className="link_footer">
+                  Acerca de The Fashion Room
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item container xs={3}>
+            <Grid item xs={12}>
+              <Typography component="div" className="title_footer">
+                Gana dinero con nosotros
+              </Typography>
+            </Grid>
+            <Grid item container direction="column">
+              <Grid item>
+                <Typography component="div" className="link_footer">
+                  Vender productos en The Fashion Room
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography component="div" className="link_footer">
+                  Vender en The Fashion Room business
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography component="div" className="link_footer">
+                  Programa de afiliados
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item container xs={3}>
+            <Grid item xs={12}>
+              <Typography component="div" className="title_footer">
+                Productos de pago
+              </Typography>
+            </Grid>
+            <Grid item container direction="column">
+              <Grid item>
+                <Typography component="div" className="link_footer">
+                  Compra con puntos
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography component="div" className="link_footer">
+                  Recarga tu saldo
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography component="div" className="link_footer">
+                  Conversor de divisas de The Fashion Room
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item container xs={3}>
+            <Grid item xs={12}>
+              <Typography component="div" className="title_footer">
+                Redes sociales
+              </Typography>
+            </Grid>
+            <Grid item container direction="column">
+              <Grid item>
+                <Typography component="div" className="link_footer">
+                  Síguenos en
+                </Typography>
+              </Grid>
+              <Grid item container spacing={2}>
+                <Grid item>
+                  <FacebookIcon />
+                </Grid>
+                <Grid item>
+                  <InstagramIcon />
+                </Grid>
+                <Grid item>
+                  <TwitterIcon />
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   );
 }
 
--- the-fashion-room/src/components/offers/Offers.jsx
+++ the-fashion-room/src/components/offers/Offers.jsx
@@ -1,47 +1,51 @@
 import React from "react";
 import OfferItem from "../OfferItem";
 import { Grid } from "@mui/material";
+import { Box } from "@mui/system";
 import calzado from "../../images/Calzado.png";
 import camisetas from "../../images/Camisetas.png";
 import deportiva from "../../images/Deportiva.png";
 import bolsos from "../../images/Bolsos.png";
+import "./Offers.css";
 
 function Offers() {
   return (
-    <Grid container align="center">
-      <Grid item xs={3}>
-        <OfferItem
-          alt={"calzado"}
-          image={calzado}
-          title={"Calzado"}
-          description={"Calzado de todo tipo"}
-        />
+    <Box className="offer_container" mb={3}>
+      <Grid container align="center">
+        <Grid item xs={3}>
+          <OfferItem
+            alt={"calzado"}
+            image={calzado}
+            title={"Calzado"}
+            description={"Calzado de todo tipo"}
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <OfferItem
+            alt={"camisetas"}
+            image={camisetas}
+            title={"Camisetas"}
+            description={"Camisetas de todo tipo"}
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <OfferItem
+            alt={"deportiva"}
+            image={deportiva}
+            title={"Ropa deportiva"}
+            description={"Ropa deportiva de todo tipo"}
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <OfferItem
+            alt={"bolsos"}
+            image={bolsos}
+            title={"Bolsos y maletas"}
+            description={"Bolsos y maletas de todo tipo"}
+          />
+        </Grid>
       </Grid>
-      <Grid item xs={3}>
-        <OfferItem
-          alt={"camisetas"}
-          image={camisetas}
-          title={"Camisetas"}
-          description={"Camisetas de todo tipo"}
-        />
-      </Grid>
-      <Grid item xs={3}>
-        <OfferItem
-          alt={"deportiva"}
-          image={deportiva}
-          title={"Ropa deportiva"}
-          description={"Ropa deportiva de todo tipo"}
-        />
-      </Grid>
-      <Grid item xs={3}>
-        <OfferItem
-          alt={"bolsos"}
-          image={bolsos}
-          title={"Bolsos y maletas"}
-          description={"Bolsos y maletas de todo tipo"}
-        />
-      </Grid>
-    </Grid>
+    </Box>
   );
 }
 
