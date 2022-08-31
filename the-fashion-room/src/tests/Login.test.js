687c44793f3a649ecc6468ae42f5ee43e30c51eb
Brayan Sanchez <brayan.sanchez.leon@correounivalle.edu.co>
2022-08-31 09:06:17 -0500

    Fix Login component test

--- the-fashion-room/src/tests/Login.test.js
+++ the-fashion-room/src/tests/Login.test.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import AuthContext from "../context/auth-context/AuthContext";
 import "@testing-library/jest-dom/extend-expect";
 import { screen, render } from "@testing-library/react";
 import Login from "../pages/login/Login";
 
+const mockSetAuth = jest.fn();
+
 test("render login", () => {
-  render(<Login />);
-  //console.log(component);
-  screen.getByText("Iniciar sesión");
+  render(
+    <AuthContext.Provider value={{ mockSetAuth }}>
+      <Router>
+        <Login />
+      </Router>
+    </AuthContext.Provider>,
+  );
+
+  //screen.getByText("Iniciar sesión");
+  expect(screen.getByText("Iniciar sesión")).toBeInTheDocument();
 });
