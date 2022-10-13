b03c0f8b003f09b4cc2631e3476a4f4c806cdcb1
Brayan Sanchez <brayan.sanchez.leon@correounivalle.edu.co>
2022-10-13 11:51:28 -0500

    Changes in queries sql file to improve database schema

--- queries.sql
+++ queries.sql
@@ -59,9 +59,30 @@ CREATE OR REPLACE FUNCTION invoice_data(inv_id INTEGER, prod_id INTEGER, quant I
 CREATE TABLE product (
     product_id INTEGER,
     product_name VARCHAR(25) NOT NULL CHECK (product_name <> ''),
+    CONSTRAINT pk_product PRIMARY KEY (product_id)
+);
+
+-- Joining table for product and product variants
+CREATE TABLE product_option (
+    product_id INTEGER,
+    product_variant_id INTEGER
+    CONSTRAINT pk_product_option PRIMARY KEY (product_id, product_variant_id),
+    CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES product (product_id),
+    CONSTRAINT fk_product_variant_id FOREIGN KEY (product_variant_id) REFERENCES product_variant (product_variant_id)
+);
+
+-- Represents all products variants
+CREATE TABLE product_variant (
+    product_id INTEGER,
+    product_variant_id SERIAL,
+    product_gender VARCHAR(25),
+    product_category VARCHAR(25),
+    product_shipping_cost INTEGER NOT NULL,
+    product_color VARCHAR(25),
     product_price INTEGER NOT NULL,
     product_stock INTEGER NOT NULL,
-    CONSTRAINT pk_product PRIMARY KEY (product_id)
+    CONSTRAINT pk_product_variant_id PRIMARY KEY (product_id, product_variant_id),
+    CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES product (product_id)
 );
 
 -- Create product_id sequence for auto increment
@@ -71,12 +92,12 @@ CREATE SEQUENCE product_product_id_seq
     OWNED BY product.product_id;
 
 -- Insert available products to the product table
-INSERT INTO product (product_id, product_name, product_price, product_stock) VALUES (nextval('product_product_id_seq'), 'Blusa', 45000, 100);
-INSERT INTO product (product_id, product_name, product_price, product_stock) VALUES (nextval('product_product_id_seq'), 'Camisa', 40000, 100);
-INSERT INTO product (product_id, product_name, product_price, product_stock) VALUES (nextval('product_product_id_seq'), 'Corbata', 20000, 100);
-INSERT INTO product (product_id, product_name, product_price, product_stock) VALUES (nextval('product_product_id_seq'), 'Pantalon', 65000, 100);
-INSERT INTO product (product_id, product_name, product_price, product_stock) VALUES (nextval('product_product_id_seq'), 'Pantaloneta', 30000, 100);
-INSERT INTO product (product_id, product_name, product_price, product_stock) VALUES (nextval('product_product_id_seq'), 'Zapatos', 90000, 100);
+INSERT INTO product (product_id, product_name) VALUES (nextval('product_product_id_seq'), 'Blusa');
+INSERT INTO product (product_id, product_name) VALUES (nextval('product_product_id_seq'), 'Camisa');
+INSERT INTO product (product_id, product_name) VALUES (nextval('product_product_id_seq'), 'Corbata');
+INSERT INTO product (product_id, product_name) VALUES (nextval('product_product_id_seq'), 'Pantalon');
+INSERT INTO product (product_id, product_name) VALUES (nextval('product_product_id_seq'), 'Pantaloneta');
+INSERT INTO product (product_id, product_name) VALUES (nextval('product_product_id_seq'), 'Zapatos');
 
 -- Create customer table
 CREATE TABLE customer (
@@ -92,42 +113,44 @@ CREATE TABLE customer (
 CREATE TABLE wishlist (
     customer_id INTEGER,
     product_id INTEGER,
+    CONSTRAINT pk_wishlist PRIMARY KEY (customer_id, product_id),
     CONSTRAINT fk_wishlist_customer FOREIGN KEY (customer_id) REFERENCES customer (customer_id),
     CONSTRAINT fk_wishlist_product FOREIGN KEY (product_id) REFERENCES product (product_id)
 );
 
--- Create invoice table
-CREATE TABLE invoice (
-    invoice_id INTEGER,
+-- Set the customer orders
+CREATE TABLE order_detail (
+    order_detail_id INTEGER,
     customer_id INTEGER NOT NULL,
     purchase_date DATE NOT NULL,
-    CONSTRAINT pk_invoice PRIMARY KEY (invoice_id),
-    CONSTRAINT fk_invoice_customer_id FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
+    CONSTRAINT pk_order_detail PRIMARY KEY (order_detail_id),
+    CONSTRAINT fk_order_detail_customer_id FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
 );
 
--- Create invoice_id sequence for auto increment
-CREATE SEQUENCE invoice_id_seq
+-- Automatic sequence for order_detail_id
+CREATE SEQUENCE order_detail_id_seq
     START WITH 1000
     INCREMENT BY 1
-    OWNED BY invoice.invoice_id;
+    OWNED BY order_detail.order_detail_id;
 
--- Create invoice_detail table containing the purchased products
-CREATE TABLE invoice_detail (
-    detail_id SERIAL,
-    invoice_id INTEGER NOT NULL,
+-- Contains the products purchased by the customer
+CREATE TABLE order_item (
+    order_item_id SERIAL,
+    order_detail_id INTEGER NOT NULL,
     product_id INTEGER NOT NULL,
-    quantity INTEGER NOT NULL,
-    total_amount INTEGER NOT NULL,
-    CONSTRAINT fk_invoice_detail_invoice_id FOREIGN KEY (invoice_id) REFERENCES invoice(invoice_id),
-    CONSTRAINT fk_invoice_detail_product_id FOREIGN KEY (product_id) REFERENCES product(product_id)
+    product_quantity INTEGER NOT NULL,
+    item_total_cost INTEGER NOT NULL,
+    CONSTRAINT pk_order_item PRIMARY KEY (order_item_id, order_detail_id),
+    CONSTRAINT fk_order_item_order_detail_id FOREIGN KEY (order_detail_id) REFERENCES order_detail(order_detail_id),
+    CONSTRAINT fk_order_item_product_id FOREIGN KEY (product_id) REFERENCES product(product_id)
 );
 
 -- Create store table
 CREATE TABLE store (
-    nit INTEGER,
-    name VARCHAR(30) NOT NULL CHECK (name <> ''),
-    address VARCHAR(30) NOT NULL CHECK (address <> ''),
-    CONSTRAINT pk_store PRIMARY KEY (nit)
+    store_nit INTEGER,
+    store_name VARCHAR(30) NOT NULL CHECK (name <> ''),
+    store_address VARCHAR(30) NOT NULL CHECK (address <> ''),
+    CONSTRAINT pk_store PRIMARY KEY (store_nit)
 );
 
 -- Create nit sequence for auto increment
@@ -137,23 +160,23 @@ CREATE SEQUENCE nit_seq
     OWNED BY store.nit;
 
 -- Insert store data to the store table
-INSERT INTO store (nit, name, address) VALUES (nextval('nit_seq'), 'THE FASHION ROOM SUR', 'CR 29B N 325-4');
-INSERT INTO store (nit, name, address) VALUES (nextval('nit_seq'), 'THE FASHION ROOM NORTE', 'CR 100 N110-2');
-INSERT INTO store (nit, name, address) VALUES (nextval('nit_seq'), 'THE FASHION ROOM ORIENTE', 'CR 5 N20-12');
-INSERT INTO store (nit, name, address) VALUES (nextval('nit_seq'), 'THE FASHION ROOM OCCIDENTE', 'CR 5 N1-2');
-INSERT INTO store (nit, name, address) VALUES (nextval('nit_seq'), 'THE FASHION ROOM SUROCCIDENTE', 'CR 7 N56-32');
+INSERT INTO store (store_nit, store_name, store_address) VALUES (nextval('nit_seq'), 'THE FASHION ROOM SUR', 'CR 29B N 325-4');
+INSERT INTO store (store_nit, store_name, store_address) VALUES (nextval('nit_seq'), 'THE FASHION ROOM NORTE', 'CR 100 N110-2');
+INSERT INTO store (store_nit, store_name, store_address) VALUES (nextval('nit_seq'), 'THE FASHION ROOM ORIENTE', 'CR 5 N20-12');
+INSERT INTO store (store_nit, store_name, store_address) VALUES (nextval('nit_seq'), 'THE FASHION ROOM OCCIDENTE', 'CR 5 N1-2');
+INSERT INTO store (store_nit, store_name, store_address) VALUES (nextval('nit_seq'), 'THE FASHION ROOM SUROCCIDENTE', 'CR 7 N56-32');
 
 -- Create a table containing the store's phone numbers
 CREATE TABLE store_phone (
-    nit INTEGER,
-    phone BIGINT NOT NULL,
-    CONSTRAINT pk_store_phone PRIMARY KEY (nit),
-    CONSTRAINT fk_store_phone FOREIGN KEY (nit) REFERENCES store(nit)
+    store_nit INTEGER,
+    store_phone BIGINT NOT NULL,
+    CONSTRAINT pk_store_phone PRIMARY KEY (store_nit),
+    CONSTRAINT fk_store_phone FOREIGN KEY (store_nit) REFERENCES store(store_nit)
 );
 
 -- Insert store's phone numbers to the store_phone table
-INSERT INTO store_phone (nit, phone) VALUES (2020, 4225136);
-INSERT INTO store_phone (nit, phone) VALUES (2021, 4225136);
-INSERT INTO store_phone (nit, phone) VALUES (2022, 4218755);
-INSERT INTO store_phone (nit, phone) VALUES (2023, 4856923);
-INSERT INTO store_phone (nit, phone) VALUES (2024, 4128745);
+INSERT INTO store_phone (store_nit, store_phone) VALUES (2020, 4225136);
+INSERT INTO store_phone (store_nit, store_phone) VALUES (2021, 4225136);
+INSERT INTO store_phone (store_nit, store_phone) VALUES (2022, 4218755);
+INSERT INTO store_phone (store_nit, store_phone) VALUES (2023, 4856923);
+INSERT INTO store_phone (store_nit, store_phone) VALUES (2024, 4128745);
