37fab4912f3bc7e28d9d5ec316e5732098a84b3b
Brayan Sanchez <brayan.sanchez.leon@correounivalle.edu.co>
2022-12-01 11:44:30 -0500

    Refactored Shopping cart component.

--- client/src/pages/shopping-cart/ShoppingCart.jsx
+++ client/src/pages/shopping-cart/ShoppingCart.jsx
@@ -15,49 +15,41 @@ function ShoppingCart() {
   const { cart, removeFromCart, clearCart } = useContext(ProductContext);
 
   return (
-    <Grid container direction="column">
-      <Grid item>
-        <Navbar />
-      </Grid>
-      <Grid item={true} sx={{ height: "auto", width: "auto", pt: 10, pb: 50 }}>
-        <Grid container spacing={2}>
-          <Grid item={true} container xs={6}>
-            <Grid item container justifyContent="center">
-              <Grid item>
-                <Button
-                  sx={{ width: 200, mr: 1 }}
-                  component={Link}
-                  to="/catalogue"
-                  variant="outlined"
-                >
-                  Regresar
-                </Button>
-              </Grid>
-              <Grid item>
-                <Button
-                  sx={{ width: 200, ml: 1 }}
-                  onClick={() => clearCart()}
-                  variant="outlined"
-                >
-                  Vaciar carrito
-                </Button>
-              </Grid>
+    <div className="container">
+      <Navbar />
+      <Grid container spacing={2}>
+        <Grid item={true} container xs={6}>
+          <Grid item container justifyContent="center">
+            <Grid item>
+              <Button
+                sx={{ width: 200, mr: 1 }}
+                component={Link}
+                to="/catalogue"
+                variant="outlined"
+              >
+                Regresar
+              </Button>
             </Grid>
-            <Grid item container justifyContent="center" direction="column">
-              {cart.map((product, index) => (
-                <Grid item key={index}>
-                  <CartItem product={product} removeFromCart={removeFromCart} />
-                </Grid>
-              ))}
+            <Grid item>
+              <Button sx={{ width: 200, ml: 1 }} onClick={() => clearCart()} variant="outlined">
+                Vaciar carrito
+              </Button>
             </Grid>
           </Grid>
-          <Grid item={true} justifyContent="center" xs={6}>
-            <OrderResume />
+          <Grid item container justifyContent="center" direction="column">
+            {cart.map((product, index) => (
+              <Grid item key={index}>
+                <CartItem product={product} removeFromCart={removeFromCart} />
+              </Grid>
+            ))}
           </Grid>
+        </Grid>
+        <Grid item={true} justifyContent="center" xs={6}>
+          <OrderResume />
         </Grid>
       </Grid>
       <Footer />
-    </Grid>
+    </div>
   );
 }
 
