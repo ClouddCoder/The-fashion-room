d2fc1d697e9e101fb0b19f320e26f3fcdc84b7b2
Brayan <brayan.sanchez.leon@correounivalle.edu.co>
2023-01-23 18:38:23 -0500

    Created drops queries for the database

---
+++ api/db/drop.sql
@@ -0,0 +1,16 @@
+DROP TABLE store_address
+DROP TABLE customer_address;
+DROP TABLE customer_phone;
+DROP TABLE store_phone;
+DROP TABLE phone;
+DROP TABLE addresses;
+DROP TABLE store;
+DROP TABLE order_item;
+DROP TABLE order_detail;
+DROP TABLE wishlist;
+DROP TABLE customer;
+DROP TABLE variant_color;
+DROP TABLE color;
+DROP TABLE variant;
+DROP TABLE gender;
+DROP TABLE category;
